2b14fb2e46847643530b9efc5af5cce03260eeff
Daniel Smanioto <daniel.smanioto@gmail.com>
2020-05-08 22:17:46 -0300

    Move to root

---
+++ js/usefull.js
@@ -0,0 +1,5 @@
+const URL_API = "https://ds-burger-server.herokuapp.com";
+
+let getUrlAPI = () => {
+  return URL_API;
+}
--- site/js/usefull.js
+++
@@ -1,5 +0,0 @@
-const URL_API = "http://localhost:80";
-
-let getUrlAPI = () => {
-  return URL_API;
-}
